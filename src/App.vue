cf7ca3e4e7ad5e24f4dd2b377e10e75bebcb1a9b
Loganmz <116796944+Loganmz@users.noreply.github.com>
2023-05-19 14:47:09 +0200

    about us

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
 import HeaderPage from './components/HeaderPage.vue'
 import FooterPage from './components/FooterPage.vue'
 import HeroPage from './components/HeroPage.vue'
+import aboutus from './components/aboutus.vue'
+import wheels from './components/wheels.vue'
+
 </script>
 
 <template>
   <HeaderPage />
   <RouterView />
   <HeroPage />
+  <aboutus />
+  <wheels />
   <FooterPage />
 </template>
---
+++ src/components/wheels.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div class="mx-auto w-full max-w-screen-lg p-4 py-6 lg:py-8">
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <div class="text-center">
+        <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-[#989898] rounded-full bg-teal-accent-400">
+            Find out more
+          </p>
+          <h2 class="mb-5 font-sans text-3xl font-bold tracking-tight text-black sm:text-4xl sm:leading-none">
+            NEW EXPERIENCE
+          </h2>
+          <button class="px-14 rounded-lg flex-1 p-3 none bg-blue font-bold text-white lg:flex-none lg:px-9px">
+          Spin It
+        </button>
+    </div>
+    
+    </div>
+    
+</template>
