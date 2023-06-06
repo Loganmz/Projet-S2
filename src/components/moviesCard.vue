2d1c30bf9a006f61f892af613a9b9d7332b7bf4c
Loganmz <116796944+Loganmz@users.noreply.github.com>
2023-06-06 17:32:15 +0200

    last

--- src/components/moviesCard.vue
+++ src/components/moviesCard.vue
@@ -7,28 +7,40 @@ const img = MoviesInfo.attach
 const urlImg = img && pb.getFileUrl(MoviesInfo, img, { thumb: '100x200' })
 </script>
 <template>
-    <div class=" m-4 flex flex-col items-center bg-gray-800 rounded-lg shadow sm:flex-row max-w-xl self-center  ">
-        <img :src="urlImg" class="pl-4 object-cover w-full h-96 rounded-lg" />
-        <div class="flex flex-col justify-between p-4 leading-normal">
-            <h2 class="mb-2 font-futura-bdcn-bt text-base font-bold tracking-tight text-gray-900 dark:text-white">{{ title
-            }}</h2>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ filmer }}</p>
-            <p class="mb-3 font-normal text-white ">{{ synopsis }}</p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ genre }}</p>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ time }}min</p>
-            <div class="flex flex-row grid-col-2">
-                <iconestar />
-                <p class="mb-3 font-normal text-white ">{{ note }}/10</p>
-                <p class="mb-3 font-normal text-white "></p>
+    <div class="flex justify-center">
+        <div class="w-2/3 max-w-xl">
+            <div class="m-4 bg-gray-800 rounded-lg shadow-md flex flex-col sm:flex-row h-auto overflow-hidden">
 
-            </div>
-            <div class="flex flex-row grid-col-2">
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ release_year }}</p>
-                <a :href="trailer" target="_blank" rel="noopener noreferrer">
-          <button class="w-48 bg-blue text-white mb-6 mt-8 font-bold py-2 px-4 rounded-full">
-            Trailer
-          </button>
-        </a>
+                <div class="w-full sm:w-1/2 relative">
+                    <div class="h-full overflow-hidden rounded-tl-lg">
+                        <img :src="urlImg" class="object-cover h-full" />
+                    </div>
+                    <div class="absolute inset-0 bg-gradient-to-r from-transparent to-black opacity-50 rounded-tl-lg">
+                    </div>
+                </div>
+
+                <div class="w-full sm:w-1/2 flex flex-col justify-between p-4">
+                    <h2 class="mb-2 text-sm font-bold text-gray-900 dark:text-white">{{ title }}</h2>
+                    <p class="mb-3 text-gray-700 dark:text-gray-400">{{ filmer }}</p>
+                    <div class="flex-grow overflow-y-auto">
+                        <p class="mb-3 text-white">{{ synopsis }}</p>
+                    </div>
+                    <p class="mb-3 text-gray-700 dark:text-gray-400">{{ genre }}</p>
+                    <p class="mb-3 text-gray-700 dark:text-gray-400">{{ time }}min</p>
+                    <div class="flex items-center mb-3">
+                        <iconestar class="w-4 h-4 mr-1" />
+                        <p class="text-white">{{ note }}/10</p>
+                    </div>
+                    <div class="flex justify-between items-center">
+
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ release_year }}</p>
+                        <a :href="trailer" target="_blank" rel="noopener noreferrer">
+                            <button class="w-48 bg-blue text-white mb-6 mt-8 font-bold py-2 px-4 rounded-full">
+                                Trailer
+                            </button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
